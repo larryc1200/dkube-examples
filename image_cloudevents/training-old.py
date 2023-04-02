e892b00f136488cecf9d2b210d1e22fc86a17062
Larry Chisvin <larry.chisvin@oneconvergence.com>
2023-04-02 09:11:25 -0600

    Enhancements

--- image_cloudevents/training-old.py
+++ image_cloudevents/training-old.py
@@ -4,28 +4,9 @@ import glob
 import cv2
 from sklearn.preprocessing import OneHotEncoder
 import tensorflow as tf
-from tensorflow import keras
 import mlflow
 
 image_types = ('.jpg', 'jpeg', '.png', '.svg')
-
-# Ignore all warnings to clean up log file
-import warnings
-warnings.filterwarnings("ignore")
-import requests
-requests.packages.urllib3.disable_warnings()
-
-# Set up parsing for the Katib inputs
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=5,
-                        help='The number of epochs for training')
-parser.add_argument('--learning_rate', type=float, default=0.01,
-                        help="learning rate for optimizer")
-args = parser.parse_args()
-
-# Image Data Functions
 
 class ImageData():
     def __init__(self):
@@ -75,8 +56,7 @@ class ImageData():
             resized_images.append(cv2.resize(each_image, new_shape, interpolation= cv2.INTER_LINEAR))
         resized_images = np.asarray(resized_images)
         return resized_images
-
-# Set up image variables
+        
 imd = ImageData()
 train_x, train_y = imd.read_classification_data("/data")
 train_y_classes, train_y = train_y
@@ -86,63 +66,27 @@ resized_train_x = resized_train_x.reshape(resized_train_x.shape[0], 200, 200, 1)
 encoder = OneHotEncoder(sparse=False)
 onehot = encoder.fit_transform(train_y.reshape(-1, 1))
 
-# Set up input hyperparameters
-NUM_EPOCHS = int(os.getenv("EPOCHS", args.epochs))
-LEARNING_RATE = args.learning_rate
+# Create the MLFlow Experiment name
 
-# Set up MLFlow Experiment
-MLFLOW_EXPERIMENT_NAME = os.getenv('DKUBE_PROJECT_NAME')
 
-if MLFLOW_EXPERIMENT_NAME:
-    exp = mlflow.get_experiment_by_name(MLFLOW_EXPERIMENT_NAME)
-    if not exp:
-        print("Creating experiment...")
-        mlflow.create_experiment(MLFLOW_EXPERIMENT_NAME)
-    mlflow.set_experiment(experiment_name=MLFLOW_EXPERIMENT_NAME)
+# Create and set MLFlow Experiment
+mlflow.create_experiment(MLFLOW_EXPERIMENT_NAME)
+mlflow.set_experiment(experiment_name=MLFLOW_EXPERIMENT_NAME)
 
 # Output directory for MLFlow
 OUTPUT_MODEL_DIR = os.getcwd()+"/model_mlflow"
 os.makedirs(OUTPUT_MODEL_DIR, exist_ok=True)
 
-# MLFlow metric logging function
-class loggingCallback(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        mlflow.log_metric("train_loss", logs["loss"], step=epoch)
-        mlflow.log_metric ("train_accuracy", logs["accuracy"], step=epoch)
-        mlflow.log_metric("val_loss", logs["val_loss"], step=epoch)
-        mlflow.log_metric ("val_accuracy", logs["val_accuracy"], step=epoch)
-        # output accuracy metric for katib to collect from stdout
-        print(f"loss={round(logs['loss'],2)}")
-        print(f"val_loss={round(logs['val_loss'],2)}")
-        print(f"accuracy={round(logs['accuracy'],2)}")
-        print(f"val_accuracy={round(logs['val_accuracy'],2)}")
-
-# Create & compile model
 model = tf.keras.models.Sequential([
   tf.keras.layers.InputLayer(input_shape=(200,200,1)),
   tf.keras.layers.Conv2D(2, 4, strides=2, padding='same', activation=tf.nn.relu),
+  tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation='linear'),
-  tf.keras.layers.Dense(20, input_dim=5, activation='linear'),
-  tf.keras.layers.Dense(10, activation='linear'),
-  tf.keras.layers.Dense(4, activation='linear'),
-  tf.keras.layers.Dense(2, activation='sigmoid')
+  tf.keras.layers.Dense(2)
 ])
-
-model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=LEARNING_RATE),
+model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
-
-# Train model and save metrics & artifacts
-print("MLFlow Run")
-with mlflow.start_run(run_name="xray") as run:
-    model.fit(x=resized_train_x, y=onehot, epochs=NUM_EPOCHS, verbose=False, validation_split=0.1, callbacks=[loggingCallback()])
-
-    # Exporting model & metrics
-    print("Model Save")
-    model.save("/model/1")
-
-    print("Artifact Save")
-    mlflow.log_artifacts(OUTPUT_MODEL_DIR)
-    print("Log Model")
-    mlflow.keras.log_model(keras_model=model, artifact_path=None)
+              
+model.fit(x=resized_train_x, y=onehot,epochs=6, verbose=True)
+model.save("/model/1")
